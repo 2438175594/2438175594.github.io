062a2187d9fb496def894a7339c5a86d1812cbf4
2438175594 <z2438175594.@163.com>
2019-08-23 10:09:00 +0800

    Site updated: 2019-08-23 10:08:59

--- dist/music.js
+++ dist/music.js
@@ -12,25 +12,25 @@ const ap = new APlayer({
 	  {
 	name: "Funky Stars",
 	artist: 'Quazar',
-	url: 'http://music.163.com/song/media/outer/url?id=428350808.mp3',
+	url: 'http://m10.music.126.net/20190823102528/3f1ed3e227de8de81e5f00d314b896fd/ymusic/259b/c32e/0a71/f588808685a3455013b14b5ec0ae015b.mp3',
 	cover: 'https://p1.music.126.net/Q14bFw6rRIhF2QijhLx8fQ==/18689498649065658.jpg?param=130y130',
       },
           {
         name: "canon in d",
         artist: 'Brian Crain',
-        url: 'http://music.163.com/song/media/outer/url?id=1091088.mp3',
+        url: 'http://m10.music.126.net/20190823102647/198ca43f8f3d61138b2c02a4a726a629/ymusic/2e49/e9f4/9f62/feacfeb028d2297061dc51fa061425d1.mp3',
         cover: 'http://p1.music.126.net/QGb9Vtyw7qHS00uEvPfM6g==/843325418547559.jpg?param=130y130',
       },
 	  {
         name: "Apologize",
         artist: 'Martin Ermen',
-        url: 'http://music.163.com/song/media/outer/url?id=4224657.mp3',
+        url: 'http://www.ytmp3.cn/down/59606.mp3',
         cover: 'http://p1.music.126.net/-_6mcI4VV5IKaiwhUAytbg==/1791104441647901.jpg?param=130y130',
       },
 	  {
         name: "River Flows in You",
         artist: 'Yiruma',
-        url: 'http://music.163.com/song/media/outer/url?id=20110049.mp3',
+        url: 'http://www.ytmp3.cn/down/56482.mp3',
         cover: 'http://p1.music.126.net/8ZRSyI0ZN_4ah8uzsNd1mA==/2324367581169008.jpg?param=130y130',
       },
       {
