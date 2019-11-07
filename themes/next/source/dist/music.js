bf300f34509ec1866666001edce044179e90affa
2438175594 <z2438175594.@163.com>
2019-11-07 13:04:54 +0800

    update

--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -6,31 +6,31 @@ const ap = new APlayer({
 	{
 	name: "Visions",
 	artist: 'Acreix',
-	url: 'http://www.ytmp3.cn/down/47082.mp3',
+	url: 'http://m10.music.126.net/20191107131144/cf5b1efe0c4704726e5e263317273ee8/ymusic/e4e2/e816/5465/85acb333dcc921758ad943f4e9eb6f92.mp3',
 	cover: 'https://p2.music.126.net/CDOS3j7zAYrvs6She437yQ==/109951163593319450.jpg?param=130y130',
       },
 	  {
 	name: "My Soul",
 	artist: 'July',
-	url: 'http://www.ytmp3.cn/down/50137.mp3',
+	url: 'http://m10.music.126.net/20191107131225/caea56c5ce34fc9592f2512ee5eea316/ymusic/0258/5659/045e/5289dde09ea3366948d534939748babe.mp3',
 	cover: 'https://p2.music.126.net/NFl1s5Hl3E37dCaFIDHfNw==/727876697598920.jpg?param=130y130',
       },
 	  {
 	name: "追光者",
 	artist: '岑宁儿',
-	url: 'http://www.ytmp3.cn/down/57711.mp3',
-	cover: 'https://onedrive.gimhoy.com/sharepoint/aHR0cHM6Ly9kYWJjb29vYWNuei1teS5zaGFyZXBvaW50LmNvbS86dTovZy9wZXJzb25hbC9wYm0yNTc5X211YWJsX2NuL0VlbnNwVml4aWQ1Tmo1WWNMSXlyY0ZnQlRMMGF5ZEZhRjFxanhkaVFDcHFlZ3c/ZT1EUGJlTm8=.mp3',
+	url: 'http://onedrive.gimhoy.com/sharepoint/aHR0cHM6Ly9kYWJjb29vYWNuei1teS5zaGFyZXBvaW50LmNvbS86dTovZy9wZXJzb25hbC9wYm0yNTc5X211YWJsX2NuL0VlbnNwVml4aWQ1Tmo1WWNMSXlyY0ZnQlRMMGF5ZEZhRjFxanhkaVFDcHFlZ3c/ZT1BSGJ4amM=.mp3',
+	cover: 'https://p2.music.126.net/ZZAQGWl9mR7g5xCyWWH3Pw==/19149094509535913.jpg?param=130y130',
       },
           {
         name: "让酒",
         artist: '摩登兄弟',
-        url: 'http://www.ytmp3.cn/down/53762.mp3',
+        url: 'http://m10.music.126.net/20191107131314/f7a235c109ce6960acf35edb7ac77d57/ymusic/6424/cc93/e872/a3f70652c7a94d7d0e1fc173fd7db5e7.mp3',
         cover: 'https://p1.music.126.net/JtRFFxXdu4TiKAhK7JT4OQ==/109951163830435640.jpg?param=130y130',
       },
 	  {
 	name: "哪吒",
 	artist: '蔡翊昇',
-	url: 'http://www.ytmp3.cn/down/58327.mp3',
+	url: 'http://m10.music.126.net/20191107131404/5942e8536baa4b868ba82e0df0a0a8e8/ymusic/d57d/6717/d47c/c5e8a190e1472483327cd016c45aeefc.mp3',
 	cover: 'https://p1.music.126.net/Za_wWwp1CRCMAesBbE994A==/109951163023780798.jpg?param=130y130',
       },
 	  {
